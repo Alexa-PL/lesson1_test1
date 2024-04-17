1968ed6cb6b0eaeaa84e63d655b94551040c98c8
Aleksandra PLatanaya <platanaya48@gmail.com>
2024-04-17 20:40:40 +0300

    second commit

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello world!");
+﻿//Console.WriteLine("Hello world!");
+
+int value = 100;//введение новой переменной value 
+Console.WriteLine(value);//и выведение ее на экран
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
